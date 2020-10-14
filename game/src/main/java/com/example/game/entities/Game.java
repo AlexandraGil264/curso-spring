081effe73fea910e412ac793f1a02d1d1faade4c
AlexandraGil264 <65333195+AlexandraGil264@users.noreply.github.com>
2020-10-14 21:14:34 +0200

    Add files via upload

---
+++ game/src/main/java/com/example/game/entities/Game.java
@@ -0,0 +1,30 @@
+package com.example.game.entities;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "GAMES")
+@Data
+public class Game {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "TITLE")
+	private String title;
+	
+	@Column(name = "DESCRIPTION")
+	private String description;
+	
+	@Column(name = "RELEASE")
+	private Date release;
+}
